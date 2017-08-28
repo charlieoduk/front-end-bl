be6ce28d87cc06cc391e1f17078d02b57b66df79
charlieoduk <odukjr@gmail.com>
2017-08-28 10:43:52 +0300

    [chore] Add user service and model

---
+++ src/app/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+import { User } from './user';
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: Http) { }
+
+  create(user: User) {
+    return this.http.post('/api/users', user, this.jwt()).map((response: Response) => response.json());
+  }
+
+  private jwt() {
+    // create authorization header with jwt token
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.token) {
+        let headers = new Headers({ 'Authorization': currentUser.token });
+        return new RequestOptions({ headers: headers });
+    }
+  }
+
+}
