8beceaa6f7a5b2fe975abc2b799fe043f9032c54
charlieoduk <odukjr@gmail.com>
2017-08-28 10:52:22 +0300

    [chore] Modify Update the required modules for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+git addimport { BootstrapModalModule } from 'ng2-bootstrap-modal';
 
 import { AppComponent } from './app.component';
+import { BucketListComponent } from './bucket-list/bucket-list.component';
+
+import { BucketlistsService } from './bucketlists.service';
+import { BucketListDetailsComponent } from './bucket-list-details/bucket-list-details.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { appRouterModule } from './app.routes';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { AlertComponent } from './alert/alert.component';
+import { AuthGuard } from './auth.guard';
+import { AlertService } from './alert.service';
+import { AuthenticationService } from './authentication.service';
+import { UserService } from './user.service';
+import { HomeComponent } from './home/home.component';
+import { ConfirmComponent } from './confirm/confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BucketListComponent,
+    BucketListDetailsComponent,
+    LoginComponent,
+    NavComponent,
+    FooterComponent,
+    AlertComponent,
+    HomeComponent,
+    ConfirmComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRouterModule,
+    FormsModule,
+    HttpModule,
+    BootstrapModalModule
   ],
-  providers: [],
+  providers: [
+    BucketlistsService,
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+  ],
+  entryComponents: [ConfirmComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
