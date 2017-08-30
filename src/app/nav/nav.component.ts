047ca4f6a24607c5c41de3070f6226d5b329f7f9
charlieoduk <odukjr@gmail.com>
2017-08-30 10:17:50 +0300

    [chore] Update nav bar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { NgForm } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
+
+// import { SearchService } from '../search.service';
+import { Bucketlist } from '../bucketlist';
+import { BucketlistsService } from '../bucketlists.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  results: Object;
+  searchTerm$ = new Subject<string>();
+  @Input() bucketlists: Bucketlist[];
+  model: any = {};
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService,
+    private bucketlistsService: BucketlistsService,
+  ) { }
 
   ngOnInit() {
   }
-
+  logoutClicked($event) {
+    this.authenticationService.logout();
+  }
+  performSearch(): void {
+    const searchTerm = this.model.search;
+    this.bucketlistsService.search(searchTerm)
+      .subscribe((data) => {
+        this.bucketlists = data;
+      });
+  }
 }
