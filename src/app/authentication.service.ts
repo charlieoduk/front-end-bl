f637cae88465795643164e051ae1c8c211100f00
charlieoduk <odukjr@gmail.com>
2017-08-30 10:22:30 +0300

    [chore] Modify authentication service

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -3,21 +3,30 @@ import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+import { AlertService } from './alert.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
 @Injectable()
 export class AuthenticationService {
     private baseUrl: string = 'https://bucketlist-api-app.herokuapp.com';
+    errorMessage: string = '';
 
-    constructor(private http: Http) { }
+    constructor(private http: Http,
+         private alertService: AlertService,
+         private router: Router
+        ) { }
 
     login(email: string, password: string) {
         return this.http.post(`${this.baseUrl}/api/v1.0/auth/login/`, JSON.stringify({ email: email, password: password }))
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
                 let user = response.json();
-                console.log(user);
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
+                }else {
+                    alert(user.message);
+                    window.location.reload();
                 }
 
                 return user;
