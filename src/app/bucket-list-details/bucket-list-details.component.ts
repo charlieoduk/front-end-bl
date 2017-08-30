1fe4e9a59c06da6cdbc86fbdb202dd67eee73a05
charlieoduk <odukjr@gmail.com>
2017-08-30 10:19:03 +0300

    [chore] Update bucket list items

--- src/app/bucket-list-details/bucket-list-details.component.ts
+++ src/app/bucket-list-details/bucket-list-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+
 import { ConfirmComponent } from '../confirm/confirm.component';
 import { DialogService } from 'ng2-bootstrap-modal';
 import { Bucketlist } from '../bucketlist';
@@ -12,6 +13,8 @@ import { BucketlistsService } from '../bucketlists.service';
   styleUrls: ['./bucket-list-details.component.css']
 })
 export class BucketListDetailsComponent implements OnInit {
+  public editableString: string;
+
   @Input() bucketlist: Bucketlist;
   model: any = {};
 
@@ -29,8 +32,8 @@ export class BucketListDetailsComponent implements OnInit {
         .get(id)
         .subscribe(p => {
           this.bucketlist = p,
-          console.log(p);
-         });
+            console.log(p);
+        });
     });
   }
 
@@ -40,14 +43,37 @@ export class BucketListDetailsComponent implements OnInit {
   gotoBucketLists() {
     window.history.back();
   }
+  isChecked(status: boolean, id: number, itemid: number) {
+    let body: any;
+    if (status) {
+      body = JSON.stringify({ 'done': false });
+      this.bucketlistsService
+        .changeStatus(body, id, itemid)
+        .subscribe(p => {
+          this.bucketlistsService.get(id).subscribe((data) => {
+            this.bucketlist = data;
+          });
+        });
+    } else {
+      body = JSON.stringify({ 'done': true });
+      this.bucketlistsService
+      .changeStatus(body, id, itemid)
+      .subscribe(p => {
+        this.bucketlistsService.get(id).subscribe((data) => {
+          this.bucketlist = data;
+        });
+      });
+    }
+    console.log(body);
+  }
   addItem() {
     this.route.params.subscribe(params => {
       const id = Number.parseInt(params['id']);
       const body = JSON.stringify(this.model);
       this.bucketlistsService
-      .addBucketlistItem(body, id)
-      .subscribe(response => this.bucketlistsService.get(id).subscribe((data) => {
-        this.bucketlist = data;
+        .addBucketlistItem(body, id)
+        .subscribe(response => this.bucketlistsService.get(id).subscribe((data) => {
+          this.bucketlist = data;
         }));
     });
   }
@@ -55,29 +81,30 @@ export class BucketListDetailsComponent implements OnInit {
   deleteItem(id: number, itemid: number) {
     const disposable = this.dialogService.addDialog(ConfirmComponent, {
       title: 'Delete Bucketlist Item',
-      message: 'Are you sure you want to delete this bucketlist'})
+      message: 'Are you sure you want to delete this bucketlist'
+    })
       .subscribe((isConfirmed) => {
-          if (isConfirmed) {
-              // alert('accepted');
-              console.log('deleting bucketlist item with id: ', id);
-              this.bucketlistsService
-              .deleteBucketlistItem(Number(id), Number(itemid))
-              .subscribe(p => {
-                this.bucketlistsService.get(id).subscribe((data) => {
-                  this.bucketlist = data;
-                });
+        if (isConfirmed) {
+          // alert('accepted');
+          console.log('deleting bucketlist item with id: ', id);
+          this.bucketlistsService
+            .deleteBucketlistItem(Number(id), Number(itemid))
+            .subscribe(p => {
+              this.bucketlistsService.get(id).subscribe((data) => {
+                this.bucketlist = data;
               });
-          }
+            });
+        }
       });
     setTimeout(() => {
       disposable.unsubscribe();
-      }, 10000);
+    }, 10000);
   }
 
   saveBucketlistDetails() {
     this.bucketlistsService
-        .save(this.bucketlist)
-        .subscribe(r => console.log(`saved!!! ${JSON.stringify(this.bucketlist)}`));
+      .save(this.bucketlist)
+      .subscribe(r => console.log(`saved!!! ${JSON.stringify(this.bucketlist)}`));
   }
 
 }
